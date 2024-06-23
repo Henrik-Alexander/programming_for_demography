c414b5793082e9c6e1ee9cf0831ab0c4699f3799
Henrik Schubert <henrik.27@hotmail.de>
2024-06-23 09:14:52 +0100

    Update the slides

--- packages/ducksay/tests/testthat/test-ducksay.R
+++
@@ -1,8 +0,0 @@
-describe("ducksay()", {
-  it("can print to the console with 'cat'", {
-    expect_output(cat(ducksay()))
-     })
-  it("can say hello to the world", {
-    expect_match(ducksay(), "hello, world")
-  })
-})
